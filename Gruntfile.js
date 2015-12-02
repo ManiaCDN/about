a3afe37a971e1da2764123fb2a23c40de2af2d82
Tom Valk <tomvalk@gmail.com>
2015-12-02 19:46:12 +0100

    Adding Grunt actions for compiling less and minify css.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,53 @@ module.exports = function (grunt) {
     "use strict";
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
-    })
+        pkg: grunt.file.readJSON('package.json'),
+
+        less: {
+            development: {
+                options: {
+                    sourceMap: true,
+                    sourceMapFilename: 'public/stylesheets/dist.css.map',
+                    sourceMapURL: '/stylesheets/dist.css.map',
+                    sourceMapBasepath: 'public',
+                    sourceMapRootpath: '/',
+                    paths: ["public/stylesheets"]
+                },
+                files: {
+                    "public/stylesheets/dist.css": "public/stylesheets/dev/main.less"
+                }
+            },
+            production: {
+                options: {
+                    sourceMap: true,
+                    sourceMapFilename: 'public/stylesheets/dist.css.map',
+                    sourceMapURL: '/stylesheets/dist.css.map',
+                    sourceMapBasepath: 'public',
+                    sourceMapRootpath: '/',
+                    paths: ["public/stylesheets"]
+                },
+                files: {
+                    "public/stylesheets/dist.css": "public/stylesheets/dev/main.less"
+                }
+            }
+        },
+
+
+        cssmin: {
+            production: {
+                files: [{
+                    expand: true,
+                    cwd: 'public/stylesheets',
+                    src: ['dist.css'],
+                    dest: 'public/stylesheets',
+                    ext: '.min.css'
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['less', 'cssmin']);
 };
