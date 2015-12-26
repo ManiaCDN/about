d7fd8abde6acdff1199235b6cfeb30128dd6af23
Tom Valk <tomvalk@gmail.com>
2015-12-26 10:56:00 +0100

    Test server controller.

--- controllers/home.js
+++ controllers/home.js
@@ -4,6 +4,13 @@ var pool = require('./../lib/database').pool;
 
 var Server = require('./../services/Server');
 
+var bouncer = require('express-bouncer')(750, 900000, 2);
+var request = require('request');
+var async = require('async');
+var moment = require('moment');
+var Address4 = require('ip-address').Address4;
+var Address6 = require('ip-address').Address6;
+
 module.exports = {};
 
 module.exports.getHome = function (req, res, next) {
@@ -26,6 +33,7 @@ module.exports.getHome = function (req, res, next) {
             // render
             res.render('home', {
                 isDonated: req.query.hasOwnProperty('thanks'),
+                isServerTested: req.query.hasOwnProperty('servertest') ? (req.query.servertest == 1) : null,
                 mirrorsOnline: mirrorsOnline,
                 mirrors: mirrors
             });
@@ -37,6 +45,79 @@ module.exports.getHome = function (req, res, next) {
         });
 };
 
-module.exports.postServertest = function (req, res, next) {
 
+
+module.exports.postServertest = function (req, res, next) {
+    // We will test the server ip4/ip6 for correct maniacdn settings.
+    // Fire get timestamp.txt on it!
+    if (! req.body.hasOwnProperty('serverAddress')) {
+        res.redirect('/');
+        return;
+    }
+    var ip = req.body.serverAddress;
+
+    // Detect 4 or 6
+    var ip4 = new Address4(ip);
+    var ip6 = new Address6(ip);
+
+    // Valid test
+    if (! ip4.isValid() && ! ip6.isValid()) {
+        res.redirect('/');
+        return;
+    }
+
+    if (ip6.isValid()) {
+        ip = '[' + ip + ']';
+    }
+
+    // Do the request!
+    let runners = [
+        {
+            url: "http://" + ip + "/timestamp.txt",
+            options: {
+                headers: {
+                    'Host': 'maniacdn.net',
+                    'User-Agent': 'ManiaCDN Bot/1.0.0'
+                },
+                timeout: 2000
+            }
+        },
+        {
+            url: "http://" + ip + "/timestamp.txt",
+            options: {
+                headers: {
+                    'Host': 'www.maniacdn.net',
+                    'User-Agent': 'ManiaCDN Bot/1.0.0'
+                },
+                timeout: 2000
+            }
+        }
+    ];
+
+    async.eachSeries(runners, function(run, callback) {
+        request(run.url, run.options, function(err, res, body) {
+            if (err || !body) {
+                return callback(false);
+            }
+
+            let serverStamp = parseInt(body.replace(new RegExp('\n', 'g'), ''));
+            let currentStamp = moment().unix();
+
+            if (isNaN(serverStamp) || res.statusCode !== 200) {
+                return callback(false);
+            }
+
+            let difference = currentStamp - serverStamp;
+
+            if (difference > 86400) {
+                // Should be updated, but it isn't. Reject!
+                return callback(false);
+            }
+
+            return callback(true);
+        });
+    }, function(result) {
+        bouncer.reset(req);
+        res.redirect('/?servertest=' + (result ? 1 : 0));
+    });
 };
--- routes/about.js
+++ routes/about.js
@@ -3,13 +3,21 @@
 
 var express = require('express');
 var router = express.Router();
+var bouncer = require('express-bouncer')(750, 900000, 2);
 
 var homeController = require('./../controllers/home');
+
+/**
+ * Bouncer action
+ */
+bouncer.blocked = function (req, res) {
+    res.status(403).send("Too many requests have been made!");
+};
 
 /**
  *  Get about home page
  */
 router.get('/', homeController.getHome);
-router.post('/servertest', homeController.postServertest);
+router.post('/servertest', bouncer.block, homeController.postServertest);
 
 module.exports = router;
